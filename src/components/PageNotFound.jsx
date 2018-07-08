3e5d9be400720470af37f094de94c5d251853cc7
Davideb94 <davide.db94@icloud.com>
2018-07-08 19:16:40 +0200

    :construction: Structured router.

---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+
+        <div>
+            <p>Sorry, couldn't find this page.</p>
+        </div>
+
+    );
+};
+
+export default PageNotFound;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import { BrowserRouter as Router, Route, Switch, Link, browserHistory } from 'react-router-dom';
+
+import Home from './components/Home.jsx';
+import PageNotFound from './components/PageNotFound.jsx';
+
+class App extends React.Component {
+    render() {
+        return(
+            <Router history={ browserHistory }>
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/will-not-match">Will Not Match</Link></li>
+                    </ul>
+
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route path="*" component={PageNotFound} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
