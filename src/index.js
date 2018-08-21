cbb6ab1bfb4b078c6c340886b6f771045860981c
Davideb94 <davide.db94@icloud.com>
2018-08-21 11:40:46 +0200

    :heavy_plus_sign: Added Firebase as a dependency.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Link, browserHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, browserHistory } from 'react-router-dom';
+import * as firebase from 'firebase';
+
 import consts from './consts.js';
 import dictionary from './dictionary.js';
 
@@ -8,6 +10,15 @@ import Home from './components/Home.jsx';
 import PageNotFound from './components/PageNotFound.jsx';
 
 
+var config = {
+    apiKey: "AIzaSyD_GYwuLH1L6gijmIzTXKTQ7y8r5CI3_4o",
+    authDomain: "bibliometrics-d9806.firebaseapp.com",
+    databaseURL: "https://bibliometrics-d9806.firebaseio.com",
+    projectId: "bibliometrics-d9806",
+    storageBucket: "bibliometrics-d9806.appspot.com",
+    messagingSenderId: "732085926993"
+};
+firebase.initializeApp(config);
 
 class App extends React.Component {
 
